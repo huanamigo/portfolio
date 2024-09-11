a0aad3dd2f51cb83bbbead12575bcf2ab7e51a80
Artur Furtak <arturfurtak5@gmail.com>
2024-09-11 23:28:33 +0200

    added projects to work page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -5,11 +5,51 @@ export default function WorkPage() {
   return (
     <main className={styles.container}>
       <ProjectTile
+        header="Weather app"
+        image="/weather.webp"
+        tags={['React', 'TypeScript', 'Mobile First']}
+        desc="First gen weather app based on Weather API with search and autocomplete. Made using TypeScript"
+        link="https://huanamigo.github.io/weather-app/"
+      />
+
+      <ProjectTile
         header="Blach Meister"
         image="/blach.webp"
         tags={['React', 'JavaScript', 'React Router']}
         desc="Simple co-op made JavaScript Instagram clone, currently only displays pages added into the folder"
         link="https://huanamigo.github.io/Blach-Meister-Plates-DB/"
+      />
+
+      <ProjectTile
+        header="Reddit clone"
+        image="/reddit.webp"
+        tags={['Next.js', 'TypeScript', 'Postres']}
+        desc="Next.js based Reddit clone app made with PostgreSQL database on Vercel"
+        link="https://next-reddit-clone-xi.vercel.app/"
+      />
+
+      <ProjectTile
+        header="Chat app"
+        image="/chat.webp"
+        tags={['Firebase', 'TypeScript', 'React']}
+        desc="Telegram-like chat app made with TS and Firebase. You can create your own account, but make sure to remember your password"
+        link="https://huanamigo.github.io/react-chat-app/"
+      />
+
+      <ProjectTile
+        header="Pomodoro app"
+        image="/pomodoro.webp"
+        tags={['React', 'TypeScript']}
+        desc="Unnecessarily accurate pomodoro timer. Starting/stopping only has 0.001s of resetting error"
+        link="https://huanamigo.github.io/Blach-Meister-Plates-DB/"
+      />
+
+      <ProjectTile
+        header="Password app"
+        image="/password.webp"
+        tags={['React', 'JavaScript', 'SCSS']}
+        desc="Simple customizable password generator made with React and SCSS"
+        link="https://huanamigo.github.io/password-generator/"
       />
     </main>
   );
