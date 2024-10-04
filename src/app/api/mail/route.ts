f7fae548373751d575b65705d864193f622adca7
Artur Furtak <arturfurtak5@gmail.com>
2024-10-04 21:52:44 +0200

    added resend and react-email package

---
+++ src/app/api/mail/route.ts
@@ -0,0 +1,23 @@
+import { EmailTemplate } from '@/components/email-template';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Acme <twojstary@resend.dev>',
+      to: ['taktycznydrugimail@gmail.com'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName: 'John' }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
