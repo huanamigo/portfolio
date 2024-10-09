84bf9925efe6b34ba2f7c034b9f1c6af3e30ab45
Artur Furtak <arturfurtak5@gmail.com>
2024-10-09 22:25:43 +0200

    updated email form

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,15 +1,27 @@
-import { EmailTemplate } from '@/components/email-template';
+import { VercelInviteUserEmail } from '@/components/email-template';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
+  const preview = {
+    username: 'alanturing',
+    userImage: `/static/vercel-user.png`,
+    invitedByUsername: 'Alan',
+    invitedByEmail: 'alan.turing@example.com',
+    teamName: 'Enigma',
+    teamImage: `/static/vercel-team.png`,
+    inviteLink: 'https://vercel.com/teams/invite/foo',
+    inviteFromIp: '204.13.186.218',
+    inviteFromLocation: 'São Paulo, Brazil',
+  };
+
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Acme <twojstary@resend.dev>',
+      from: 'Acme <onboarding@resend.dev>',
       to: ['taktycznydrugimail@gmail.com'],
       subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
+      react: VercelInviteUserEmail(preview),
     });
 
     if (error) {
